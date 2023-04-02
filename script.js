7b44c641f6341836dd4cc121a48346ee768c2312
anano303 <124179773+anano303@users.noreply.github.com>
2023-04-02 11:02:20 +0400

    commit

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ const error = document.getElementById('error');
 form.addEventListener('submit', e => {
     e.preventDefault()
     
-    if(!email.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/))   {
+    if(!email.value.match(/^[A-Za-z\._\-0-9][ა-ჰ]*[@][A-Za-z][ა-ჰ]*[\.][ა-ჰ][a-z]{2,4}$/))   {
         error.style.display = 'flex';
     }else
-        error.style.display = 'none';
+        error.style.display = 'none'; 
     })
     
     
